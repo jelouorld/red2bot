3ba5129bd4b2bd7cdd6e14f51e1e4b503969d9b0
Andrei Enache <online@autgraph.com>
2025-09-03 12:53:04 +0200

    Add useful shell aliases

---
+++ ini.sh
@@ -0,0 +1,26 @@
+alias e=echo
+
+
+# terraform aliases
+alias taa="terraform apply -auto-approve"
+alias tda="terraform destroy -auto-approve"
+alias tp="terraform plan"
+alias tom="terraform output -raw medar_fetcher_lambda_url"
+alias tob="terraform output -raw red2bot_url"
+
+# git aliases 
+alias gs="git status"
+alias gp="git push"
+alias gr="git restore ."
+alias gl="git pull"
+alias gam="git add . && git commit -m "
+
+# aws aliases
+alias ali="aws lambda invoke --function-name "
+alias all="aws lambda list-functions"
+
+
+# some env vars:
+export PYTHONDONTWRITEBYTECODE=1
+
+
