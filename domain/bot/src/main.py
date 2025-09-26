b81992973870424f6ae1b0dc55a6a8f602911bf1
Andrei Enache <online@autgraph.com>
2025-09-26 08:45:25 +0200

    Added basic scafolding

--- domain/bot/src/main.py
+++ domain/bot/src/main.py
@@ -7,16 +7,19 @@ ddb = boto3.resource("dynamodb")
 table = ddb.Table("products")
 
 
-@router.route("/products")
-def products():
+@router.route("/init", method="POST")
+def init():
+    # create a session_id 
+    # return a session_id 
     pass
 
 
-def process(event: dict):
-    return {"event": event}
-    # return {'event': table.scan().get('Items', [])}
+@router.route("/chat/{session_id}", method="POST")
+def chat(session_id: str):
+    # execute all the langchain thing and return the response
+    pass
+
 
 
 def lambda_entrypoint(event: dict, context: dict):
-
     return router.dispatch(event)
